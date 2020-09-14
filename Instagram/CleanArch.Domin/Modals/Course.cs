763a1e5164fb75de75f0fb85fcd576cccf260e76
Mehdi Taherabadi <MehdiTaherabadi@outlook.com>
2020-09-14 16:41:35 +0430

    Create Project Domin And Context

---
+++ Instagram/CleanArch.Domin/Modals/Course.cs
@@ -0,0 +1,14 @@
+﻿namespace CleanArch.Domin.Modals
+{
+    public class Course
+    {
+        #region propertious
+
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+        public string Descerptions { get; set; }
+        public string ImageUrl { get; set; }
+
+        #endregion
+    }
+}
---
+++ Instagram/CleanArch.Infra.Data/Context/InstagramDBContext.cs
@@ -0,0 +1,19 @@
+﻿using CleanArch.Domin.Modals;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArch.Infra.Data.Context
+{
+    public class InstagramDBContext : DbContext
+    {
+        #region constrctor
+
+        public InstagramDBContext(DbContextOptions<InstagramDBContext> options) :base(options)
+        {
+            
+        }
+
+        #endregion
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
