a0d0cb202043865be30a7ae894367ce870552b29
Mehdi Taherabadi <MehdiTaherabadi@outlook.com>
2020-09-16 15:13:29 +0430

    Add Controllers In Course

---
+++ Instagram/Instargram.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Instargram.Mvc.Controllers
+{
+    public class CourseController : Controller
+    {
+
+
+
+        public IActionResult Index()
+        {
+
+
+
+            return View();
+        }
+    }
+}
