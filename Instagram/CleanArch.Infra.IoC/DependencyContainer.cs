6e0c9b19c12ac6168571a636da95be3ff2f362fb
Mehdi Taherabadi <MehdiTaherabadi@outlook.com>
2020-09-16 12:12:58 +0430

    Add Dependency In Layer IoC

---
+++ Instagram/CleanArch.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,17 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.Services;
+using CleanArch.Domin.Interfaces;
+using CleanArch.Infra.Data.Repository;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArch.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection service)
+        {
+            service.AddScoped<ICourseServices, CourseServics>();
+            service.AddScoped<ICourseRepository, CourseRepository>();
+        }
+    }
+}
