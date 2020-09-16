925b92a0c8c2edaea5a4750e1955006be591a9da
Mehdi Taherabadi <MehdiTaherabadi@outlook.com>
2020-09-16 15:42:49 +0430

    Add Course Image In Project

--- Instagram/Instargram.Mvc/Controllers/CourseController.cs
+++ Instagram/Instargram.Mvc/Controllers/CourseController.cs
@@ -1,18 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModel;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Instargram.Mvc.Controllers
 {
     public class CourseController : Controller
     {
+        private ICourseServices courseServices;
 
-
+        public CourseController(ICourseServices courseServices)
+        {
+            this.courseServices = courseServices;
+        }
 
         public IActionResult Index()
         {
-
-
-
-            return View();
+            CourseViewModel model =  courseServices.GetCourse();
+            return View(model);
         }
     }
 }
